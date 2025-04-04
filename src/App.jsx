5c3fed94d6fdca6c0251f045021dcdb127282896
yoonsun ahn <yoonsun0401@naver.com>
2025-04-04 17:25:15 +0900

    uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import {useState} from "react";
+import React, {useState} from "react";
+import {v4 as uuid4} from "uuid";
 
 function App() {
   const [items, setItems] = useState([
@@ -8,11 +9,13 @@ function App() {
     {id:3,title:'파이썬 독학',date:'2025-01-10',count:0, isOpen:true, modal:false}
     ]
   )
-  const [newItem,setNewItem] = useState('')
   const clickCount = (id) =>{
     setItems(items.map((res)=>
       res.id === id ? { ...res, count: res.count+1 } : res
     ))
+  }
+  const deleteItem = (id) =>{
+    setItems(items.filter((res)=>res.id !== id))
   }
   const toggleTitle = () =>{
     setItems(items.map((res)=>
@@ -22,18 +25,18 @@ function App() {
   const sortItem = () =>{
     setItems([...items].sort((a,b)=>a.title.localeCompare(b.title)
     ))
-    console.log(setItems)
   }
   const openModal = (id) =>{
     setItems(items.map((res)=>
       res.id === id ? {...res, modal: !res.modal} : res))
   }
+  const [newItem,setNewItem] = useState('') //input 입력값을 저장하는 상태 <input value={newItem} />
   const handleChange = (e) =>{
-    setNewItem(e.target.value)
+    setNewItem(e.target.value) //입력된 값을 상태(newItem)에 업데이트
   }
   const handleClick = () =>{
-    const today = new Date().toDateString()
-    const newId = items.length>0 ? items[items.length-1].id + 1 : 1;
+    const today = new Date().toISOString().split("T")[0];
+    const newId = uuid4();
     const newItemObject = {
       id:newId,
       title:newItem,
@@ -43,8 +46,11 @@ function App() {
       modal:false
     }
     setItems([...items,newItemObject])
+    console.log(...items)
+    console.log(items)
     setNewItem("")
   }
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -55,17 +61,21 @@ function App() {
         <div className="list" key={item.id} onClick={()=>openModal(item.id)}>
           <h4>
             <span onClick={item.id === 1 ? toggleTitle : undefined}>{ item.id === 1 && !item.isOpen ? '겨울 코트 추천' :item.title }</span>
-            <span onClick={()=>clickCount(item.id)}> ❤️</span> {item.count}
+            <span onClick={(event)=>{event.stopPropagation();clickCount(item.id)}}> ❤️</span> {item.count}
           </h4>
           <h4>{item.date}</h4>
+          <button onClick={(event)=>{event.stopPropagation();deleteItem(item.id)}} className="delete-btn">삭제</button>
           {item.modal && <Modal item={item}/>}
         </div>
       ))}
-      <input type="text" value={newItem} onChange={handleChange}></input>
+      <input type="text" value={newItem} onChange={handleChange} onKeyDown={(event)=>event.key === "Enter" && handleClick()}></input>
       <button onClick={handleClick}>추가</button>
     </div>
   )
 }
+  class Modal0 extends React.Component{
+    render
+  }
 
 function Modal({item}){
   return (
